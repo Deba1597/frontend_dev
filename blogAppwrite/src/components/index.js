16bcca0cf2d8c101773b508a11a6700ca045c223
Deba1597 <jbdebasishbehera@gmail.com>
2024-03-12 19:38:19 +0530

    update code in header footer  logoutbtn

--- blogAppwrite/src/components/index.js
+++ blogAppwrite/src/components/index.js
@@ -1,8 +1,13 @@
 import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
 import Footer from "./Footer/Footer";
-
+import Container from "./container/Container";
+import Logo from "./Logo";
 
 export {
     Header, 
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
